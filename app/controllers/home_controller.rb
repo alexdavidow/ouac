bae848daa0dbb62272eb4a5a0b3f1b730f4096c6
alexdavidow <alex.davidow@gmail.com>
2013-07-31 18:02:02 -0400

    added home view pages and methods in controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,16 @@ class HomeController < ApplicationController
   def index
     render :index
   end
+
+  def menu
+    render :menu
+  end
+
+  def about
+    render :about
+  end
+
+  def contact
+    render :contact
+  end
 end
